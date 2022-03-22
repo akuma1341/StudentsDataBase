9ba528a5bbcc029ea6c5598deeba3560924fb3a5
akuma1341 <akuma1341@gmail.com>
2022-03-22 19:36:57 +0300

    Added gitignore

--- SQL/filldata.sql
+++ SQL/filldata.sql
@@ -1,115 +1,118 @@
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Alex', 'Smith', '2000-01-01');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('John', 'Smith', '1999-02-20');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('James', 'Jones', '1995-03-15');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Robert', 'Taylor', '1993-04-16');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Michael', 'Williams', '2002-05-11');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('William', 'Brown', '2006-06-12');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('David', 'White', '2007-07-02');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Richard', 'Harris', '1991-08-03');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Joseph', 'Martin', '2000-09-22');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Thomas', 'Davies', '1993-10-24');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Charles', 'Wilson', '1994-11-19');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Mary', 'Cooper', '1996-12-18');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Patricia', 'Evans', '1997-01-19');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Jennifer', 'King', '1998-02-18');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Linda', 'Thomas', '2001-03-17');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Elizabeth', 'Baker', '2002-04-16');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Barbara', 'Green', '2002-05-15');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Susan', 'Wright', '1993-06-14');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Jessica', 'Johnson', '1990-07-13');
-INSERT INTO students.students (first_name, last_name, birth_date) VALUES ('Sarah', 'Edwards', '1992-08-10');
+INSERT INTO students.students (first_name, last_name, birth_date)
+VALUES ('Alex', 'Smith', '2000-01-01'),
+       ('John', 'Smith', '1999-02-20'),
+       ('James', 'Jones', '1995-03-15'),
+       ('Robert', 'Taylor', '1993-04-16'),
+       ('Michael', 'Williams', '2002-05-11'),
+       ('William', 'Brown', '2006-06-12'),
+       ('David', 'White', '2007-07-02'),
+       ('Richard', 'Harris', '1991-08-03'),
+       ('Joseph', 'Martin', '2000-09-22'),
+       ('Thomas', 'Davies', '1993-10-24'),
+       ('Charles', 'Wilson', '1994-11-19'),
+       ('Mary', 'Cooper', '1996-12-18'),
+       ('Patricia', 'Evans', '1997-01-19'),
+       ('Jennifer', 'King', '1998-02-18'),
+       ('Linda', 'Thomas', '2001-03-17'),
+       ('Elizabeth', 'Baker', '2002-04-16'),
+       ('Barbara', 'Green', '2002-05-15'),
+       ('Susan', 'Wright', '1993-06-14'),
+       ('Jessica', 'Johnson', '1990-07-13'),
+       ('Sarah', 'Edwards', '1992-08-10');
 
-INSERT INTO students.subjects (name) VALUES ('Math');
-INSERT INTO students.subjects (name) VALUES ('Biology');
-INSERT INTO students.subjects (name) VALUES ('Physics');
-INSERT INTO students.subjects (name) VALUES ('English');
-INSERT INTO students.subjects (name) VALUES ('Geography');
-INSERT INTO students.subjects (name) VALUES ('Marketing');
+INSERT INTO students.subjects (name)
+VALUES ('Math'),
+       ('Biology'),
+       ('Physics'),
+       ('English'),
+       ('Geography'),
+       ('Marketing');
 
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (1, 1, 5);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (1, 2, 6);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (1, 3, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (1, 4, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (2, 2, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (2, 4, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (2, 1, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (2, 3, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (2, 5, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (3, 1, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (3, 5, 1);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (3, 2, 4);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (3, 3, 5);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (3, 4, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (3, 6, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (4, 2, 2);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (4, 3, 2);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (4, 1, 5);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (4, 6, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (5, 4, 3);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (5, 5, 4);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (5, 1, 4);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (5, 3, 3);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (5, 6, 5);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (6, 1, 4);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (6, 3, 5);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (6, 5, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (6, 6, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (7, 2, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (7, 4, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (7, 6, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (8, 3, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (8, 5, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (8, 1, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (8, 2, 6);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (9, 1, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (9, 5, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (9, 2, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (9, 3, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (9, 6, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (10, 1, 10);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (10, 5, 1);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (10, 2, 5);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (10, 3, 3);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (10, 6, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (11, 2, 1);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (11, 3, 3);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (11, 4, 4);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (11, 5, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (11, 6, 6);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (12, 3, 3);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (12, 5, 5);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (12, 1, 6);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (12, 2, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (13, 3, 6);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (13, 4, 6);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (13, 4, 6);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (13, 5, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (14, 4, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (14, 5, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (14, 1, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (14, 2, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (14, 3, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (14, 6, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (15, 1, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (15, 4, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (15, 2, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (15, 3, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (15, 5, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (15, 6, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (16, 3, 9);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (16, 5, 3);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (16, 2, 5);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (17, 3, 4);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (17, 4, 5);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (17, 1, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (17, 6, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (18, 1, 6);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (18, 4, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (18, 5, 7);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (18, 3, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (18, 6, 4);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (19, 4, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (19, 5, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (19, 6, 8);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (20, 2, 10);
-INSERT INTO students.exam_marks (id_student, id_subject, mark) VALUES (20, 4, 6);
+INSERT INTO students.exam_marks (id_student, id_subject, mark)
+VALUES (1, 1, 5),
+       (1, 2, 6),
+       (1, 3, 7),
+       (1, 4, 8),
+       (2, 2, 7),
+       (2, 4, 8),
+       (2, 1, 8),
+       (2, 3, 8),
+       (2, 5, 8),
+       (3, 1, 9),
+       (3, 5, 1),
+       (3, 2, 4),
+       (3, 3, 5),
+       (3, 4, 7),
+       (3, 6, 9),
+       (4, 2, 2),
+       (4, 3, 2),
+       (4, 1, 5),
+       (4, 6, 7),
+       (5, 4, 3),
+       (5, 5, 4),
+       (5, 1, 4),
+       (5, 3, 3),
+       (5, 6, 5),
+       (6, 1, 4),
+       (6, 3, 5),
+       (6, 5, 8),
+       (6, 6, 7),
+       (7, 2, 7),
+       (7, 4, 7),
+       (7, 6, 8),
+       (8, 3, 8),
+       (8, 5, 8),
+       (8, 1, 9),
+       (8, 2, 6),
+       (9, 1, 9),
+       (9, 5, 9),
+       (9, 2, 8),
+       (9, 3, 7),
+       (9, 6, 9),
+       (10, 1, 10),
+       (10, 5, 1),
+       (10, 2, 5),
+       (10, 3, 3),
+       (10, 6, 7),
+       (11, 2, 1),
+       (11, 3, 3),
+       (11, 4, 4),
+       (11, 5, 7),
+       (11, 6, 6),
+       (12, 3, 3),
+       (12, 5, 5),
+       (12, 1, 6),
+       (12, 2, 8),
+       (13, 3, 6),
+       (13, 4, 6),
+       (13, 4, 6),
+       (13, 5, 8),
+       (14, 4, 7),
+       (14, 5, 8),
+       (14, 1, 8),
+       (14, 2, 9),
+       (14, 3, 9),
+       (14, 6, 8),
+       (15, 1, 8),
+       (15, 4, 9),
+       (15, 2, 9),
+       (15, 3, 9),
+       (15, 5, 9),
+       (15, 6, 9),
+       (16, 3, 9),
+       (16, 5, 3),
+       (16, 2, 5),
+       (17, 3, 4),
+       (17, 4, 5),
+       (17, 1, 7),
+       (17, 6, 7),
+       (18, 1, 6),
+       (18, 4, 7),
+       (18, 5, 7),
+       (18, 3, 8),
+       (18, 6, 4),
+       (19, 4, 8),
+       (19, 5, 8),
+       (19, 6, 8),
+       (20, 2, 10),
+       (20, 4, 6);
